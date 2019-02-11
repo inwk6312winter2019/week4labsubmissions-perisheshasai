1c4bb0b0e7070001941271d2d494f23d12524ddf
sheshasai <perisheshasai95@gmail.com>
2019-02-10 20:45:37 -0400

    task2

---
+++ t2.py
@@ -0,0 +1,19 @@
+from math import *
+class point:
+        def __init__(self,x,y):
+                self.x=x
+                self.y=y
+class rectangle:
+        def __init__(self,width,height):
+                self.width=width
+                self.height=height
+                self.corner=(x1.x,x1.y)
+def find_center():
+	x1.x= (x2.width/2)+x1.x
+	x1.y= (x2.height/2)+x1.y
+	print (x1.x, x1.y)
+x1=point(2,3)
+x2=rectangle(100,200)
+
+find_center()
+
